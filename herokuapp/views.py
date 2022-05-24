5a5eb5c87ed983426960d787a6aa7b0dc7410450
Michal Dobrzycki <dobrzycki@gmail.com>
2022-05-24 19:10:45 +0200

    Zmieniono strone główną

--- herokuapp/views.py
+++ herokuapp/views.py
@@ -7,5 +7,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     now = datetime.now()
-    html = "<html><body>Aktualna godzina %s.</body></html>" % now
+    html = "<html><body> Test deploymentu <hr> Aktualna godzina %s.</body></html>" % now
     return HttpResponse(html)
